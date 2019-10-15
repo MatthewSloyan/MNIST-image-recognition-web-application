2b57383882ec80fa67bc7810fa122649f96a5e39
Matthew Sloyan <g00348036@gmit.ie>
2019-10-15 18:07:05 +0100

    Fixed image plotting display and started to print image to console to learn more about how images work.

--- ImageReader.py
+++ ImageReader.py
@@ -2,15 +2,13 @@
 
 import gzip
 import numpy as np
-#%matplotlib inline
 import matplotlib.pyplot as plt
-#plt.use("gtk")
 
 # Open the MNIST dataset of 60000 training images.
 with gzip.open('MNIST_Images/t10k-images-idx3-ubyte.gz', 'rb') as f:
     file_content = f.read()
 
-print(type(file_content))
+print("File Type:", type(file_content))
 
 # Print out the first four bytes.
 # b'\x00\x00\x08\x03' is the output which correlates with the MNIST website results.
@@ -18,14 +16,23 @@ print(file_content[0:4])
 
 # Adapted from: https://stackoverflow.com/questions/51220161/how-to-convert-from-bytes-to-int
 # Convert bytes to a 32 bit integer using big-endian and little-endian.
-print(int.from_bytes(file_content[0:4], byteorder='big')) #2051
-print(int.from_bytes(file_content[0:4], byteorder='little')) #50855936
+print("Big-endian:", int.from_bytes(file_content[0:4], byteorder='big')) #2051
+print("Little-endian:", int.from_bytes(file_content[0:4], byteorder='little')) #50855936
 
 # Display a single image from the dataset
-image = ~np.array(list(file_content[16:800])).reshape(28,28).astype(np.uint8)
+image = ~np.array(list(file_content[800:1584])).reshape(28,28).astype(np.uint8)
+
+for x in image[0:28]:
+    for y in image[0:28]:
+        if y.any() > 127:
+            print("0", end=" ")
+        else: 
+            print(".", end=" ")
+    print()
 
 # Display plot of image
 plt.imshow(image, cmap='gray')
+plt.show()
 
 # Open the label dataset like before.
 with gzip.open('MNIST_Images/t10k-labels-idx1-ubyte.gz', 'rb') as f:
