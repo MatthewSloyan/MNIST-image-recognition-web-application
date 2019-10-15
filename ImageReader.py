eb77c6bb91188de5a83315141b433357dd127888
Matthew Sloyan <g00348036@gmit.ie>
2019-10-15 18:22:39 +0100

    Started to add code to train the neural network using the training dataset through Keras. This will require testing.

--- ImageReader.py
+++ ImageReader.py
@@ -1,13 +1,25 @@
 # Adapted from: https://docs.python.org/3/library/gzip.html
 
+import keras as kr
 import gzip
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Open the MNIST dataset of 60000 training images.
+# Test code to learn more about the MNIST dataset, how it works etc.
+# ======================
+
+# Open the MNIST dataset of 10000 testing images.
 with gzip.open('MNIST_Images/t10k-images-idx3-ubyte.gz', 'rb') as f:
     file_content = f.read()
 
+# Open the label dataset like before.
+with gzip.open('MNIST_Images/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+    labels = f.read()
+
+# Get the label for a specific image (Expected result = 4)
+print(int.from_bytes(labels[12:13], byteorder="big"))
+
+# Print the file type of the dataset - bytes
 print("File Type:", type(file_content))
 
 # Print out the first four bytes.
@@ -34,14 +46,30 @@ for x in image[0:28]:
 plt.imshow(image, cmap='gray')
 plt.show()
 
-# Open the label dataset like before.
-with gzip.open('MNIST_Images/t10k-labels-idx1-ubyte.gz', 'rb') as f:
-    labels = f.read()
+# Test code to train neural network, this will be then later converted to a Jupyter Notebook.
+# =========================
 
-# Get the label for a specific image (Expected result = 4)
-print(int.from_bytes(labels[12:13], byteorder="big"))
+# Start a neural network, building it by layers.
+model = kr.models.Sequential()
 
+# Add a hidden layer with 1000 neurons and an input layer with 784.
+model.add(kr.layers.Dense(units=600, activation='linear', input_dim=784))
+model.add(kr.layers.Dense(units=400, activation='relu')) # relu = Rectified Linear Unit.
+# Add a three neuron output layer.
+model.add(kr.layers.Dense(units=10, activation='softmax'))
 
+# Build the graph.
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+with gzip.open('MNIST_Images/train-images-idx3-ubyte.gz', 'rb') as f:
+    train_img = f.read()
+
+with gzip.open('MNIST_Images/train-labels-idx1-ubyte.gz', 'rb') as f:
+    train_lbl = f.read()
+
+# The bitwise operator ~ (tilde) is a complement operator. It takes one bit operand and returns its complement. If the operand is 1, it returns 0, and if it is 0, it returns 1
+train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8) / 255.0
+train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
+inputs = train_img.reshape(60000, 784)
 
 
