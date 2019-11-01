da9bf4ec4b3aac83017f36fc93de5c1efa1eb11a
Matthew Sloyan <g00348036@gmit.ie>
2019-11-01 17:38:52 +0000

    I have converted the canvase to a base64 image (JSON) which is sent to the flask server. This is read in and stripped for decoding.

--- flaskWebApplication/flaskWebApp.py
+++ flaskWebApplication/flaskWebApp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, escape, request
+import base64
 
 app = Flask(__name__)
 
@@ -9,4 +10,18 @@ def hello():
 
 @app.route("/home")
 def homePage():
+    return render_template("home.html")
+
+@app.route("/predictImage", methods=['POST'])
+def predictImage():
+    # Get the base64 image from the request
+    jsonData = request.get_json()
+
+    # get the base64 binary out of the json data so it can be decoded.
+    # base64 data follows this format "data:image/png;base64,iVBORw0KGgoA..."
+    # so strip out from base64, onwards.
+    base64_data = jsonData['data'].split('base64,')[1]
+
+    print(base64_data)
+
     return render_template("home.html")
