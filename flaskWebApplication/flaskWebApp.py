fa73e5513a9bcedad704e6a79cb8424d5da799a2
Matthew Sloyan <g00348036@gmit.ie>
2019-11-16 14:20:13 +0000

    Commented server code and removed test code. Prediction works correctly now.

--- flaskWebApplication/flaskWebApp.py
+++ flaskWebApplication/flaskWebApp.py
@@ -32,67 +32,37 @@ def predictImage():
     # https://www.w3schools.com/python/ref_string_split.asp
     base64_data = jsonData['data'].split('base64,')[1]
 
-    # Using the base64 string decode and write to a png file in the root directory
+    # Using the base64 string decoder decode base64 image.
     # I will later create dynamic file names so that multiple users could predict images at once.
-    # wb+ creates the file from scratch
-    # There's no need to close the writer as it's automatically done when exiting with statement.
     # Code adapted from and uses the imported base64 library: https://stackoverflow.com/questions/16214190/how-to-convert-base64-string-to-image
     imgdata = base64.b64decode(base64_data)
 
-    img = Image.open(BytesIO(imgdata))
-    #img = img.convert('L')
-    #img = PIL.ImageOps.invert(img)
-    img.save("image.png")
-
     # Open the image convert to bytes.
-    # img = Image.open(BytesIO(imgdata))
-    # # Save the image in bytes.
-    # img = img.save("image.png")
-
-    # inverted_image = PIL.ImageOps.invert(img)
-    # inverted_image.save("image.png")
-
-    # Use openCV to read in the image.
-    gray = cv2.imread("image.png")
-    # Grayscale the image.
-    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
-    # resize the images and invert it (black background)
-    #gray = cv2.resize(255-gray, (28, 28))
-
-    (thresh, gray) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    # # save the processed images
-    cv2.imwrite("image_Test.png", gray)
-
-    #cv2.imshow('image', gray)
-
-    # with open('testImage.png', 'wb+') as f:
-    #     f.write(imgdata)
-
     # Read RGB image and convert to grayscale E.g single channel (L)
     # Using the Pillow (PIL) library which is a Python Imaging Library by Fredrik Lundh and Contributors.
     # https://pillow.readthedocs.io/en/stable/
-    #img = Image.open('testImage.png').convert('L')
-    #print(img)
+    img = Image.open(BytesIO(imgdata)).convert('L')
+    #img.save("image.png")
 
     # The problem I initally had was that I was using a black pen in the HTML so when convert the 
     # image to greyscale there would be no distinct values (E.g All 0's) To solve this I have used a strong red pen which solves the problem.
+    # However this wasn't enough, as the image would be still gray and not strong enough for comparision.
+    # From my experience as a graphic designer I knew that increasing the threshold of an image would solve this.
+    # I wanted to use all the methods from Pillow's so I researched how to increase the threshold of an image 
+    # and found the point method which uses a lambda to increase the threshold if not black, so grey becomes solid white.
+    # Code adapted from https://www.geeksforgeeks.org/python-pil-image-point-method/
+    threshold = 0  
+    img = img.point(lambda p: p > threshold and 255) 
+    #img.save("image_Test.png")
     
     # reshape image data for use in neural network
     # Creates a two dimensional array of the 784 bytes in the image (28 x 28)
-    #imgGray = ~np.array(img).reshape(1, 784).astype(np.uint8) / 255
-    #img = np.ndarray.flatten(np.array(img).reshape(1, 784).astype(np.uint8) / 255.0)
-    imgGray = np.ndarray.flatten(np.array(gray)).reshape(1, 784).astype("float32") / 255
-
-    #grayArray = (np.array(grayImage).reshape(1, 28, 28, 1))
-    
-    #print(img)
+    imgGray = np.ndarray.flatten(np.array(img)).reshape(1, 784).astype("float32") / 255
 
     counter = 0
 
-    #Print Contents
+    #Print Contents test
     for i in imgGray[0]:
-        #print(i,end=" ")
         if i > 0:
            print(".",end="")
         else:
